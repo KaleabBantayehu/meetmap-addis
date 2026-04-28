863ffebd164dc89dff5572c6ae7b1615ac5b8343
Kaleab Bantayehu <kaleabbantayehu@gmail.com>
2026-04-28 13:45:20 +0300

    setup main navigation and routing basic page structure

---
+++ lib/features/explore/screens/explore_screen.dart
@@ -0,0 +1,14 @@
+// TODO: implement explore screen
+
+import 'package:flutter/material.dart';
+
+class ExploreScreen extends StatelessWidget {
+  const ExploreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Home Screen")),
+    );
+  }
+}
---
+++ lib/features/places/screens/add_place_screen.dart
@@ -0,0 +1,1 @@
+// TODO: implement add place screen
